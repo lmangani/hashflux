4f4d368816d1735c7b360aa7dc83e6f6deb833d1
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-04-11 05:37:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const hashFlux = require('./hashflux');
 
-var options = { servers: [ '127.0.0.1t:8086' ], port: 3000 };
+var options = { servers: [ '127.0.0.1:8086' ], port: 3000 };
 var server = new hashFlux(options);
