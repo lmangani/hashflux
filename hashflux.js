972f8701fd544ceb743393c4ae4581a2c7be3cf3
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-04-14 19:08:40 +0200

    latest snap

--- hashflux.js
+++ hashflux.js
@@ -1,5 +1,7 @@
 var HashRing = require('hashring');
 const Theodore = require('theodore')
+const extend = require("xtend");
+
 
 module.exports = function HashFlux(options) {
   if (!options||!options.servers) options = { servers: ['http://127.0.0.1:8089'] };
@@ -21,10 +23,10 @@ module.exports = function HashFlux(options) {
         data: body
       }, (err, resp) => {
         if (err) {
-          console.log('[ERROR]', err.message)
+          if (options.debug) console.log('[ERROR]', err.message)
        	  reject;
         }
-        console.log(resp)
+        if (options.debug) console.log('API combo element',resp)
         resolve(resp.body || {} );
       })
 
@@ -35,8 +37,8 @@ module.exports = function HashFlux(options) {
   self.app.post('/write*', (req, res) => {
     var key = Object.keys(req.body)[0];
     var node = self.ring.get(key);
-    console.log('HASHRING', node, key);
-    console.log('WRITE SHARD',node + req.url, req.body)
+    if (options.debug) console.log('HASHRING', node, key);
+    if (options.debug) console.log('WRITE SHARD',node + req.url, req.body)
     self.req({
       method: 'POST',
       url: node + req.url,
@@ -44,10 +46,10 @@ module.exports = function HashFlux(options) {
       data: req.body
     }, (err, resp) => {
       if (err) {
-        console.log('[ERROR]', err.message)
+        if (options.debug) console.log('[ERROR]', err.message)
         return res.send(err.message, 404)
       }
-      console.log(resp)
+      if (options.debug) console.log('WRITE response',resp)
       return res.send(resp, 200)
     })
   })
@@ -55,12 +57,12 @@ module.exports = function HashFlux(options) {
   /* QUERY Handler */
   self.app.post('/query*', (req, res) => {
     var key = req.body.match(/FROM ([^WHERE|^.]+)/)[0].split('"').join('');
-    var db = req.body.match(/ database /i);
+    var db = req.body.match(/database/i);
     if (key && !db) {
 	/* sharded query */
 	    var node = self.ring.get(key);
-	    console.log('HASHRING', node, key);
-	    console.log('RING QUERY',node, req.body)
+	    if (options.debug) console.log('HASHRING', node, key);
+	    if (options.debug) console.log('RING QUERY',node, req.body)
 	    self.req({
 	      method: 'POST',
 	      url: node + req.url,
@@ -68,20 +70,23 @@ module.exports = function HashFlux(options) {
 	      data: req.body
 	    }, (err, resp) => {
 	      if (err) {
-	        console.log('[ERROR]', err.message)
+	        if (options.debug) console.log('[ERROR]', err.message)
 	        return res.send(err.message, 404)
 	      }
-	      console.log(resp)
+	      if (options.debug) console.log('QUERY response',resp)
 	      return res.send(resp, 200)
 	    })
     } else {
 	/* global query */
 	    Promise.all( servers.map(server => apiCall(req.body,req.url,server) )).then((combo) => {
-	      console.log(combo);
-	      return res.send(combo, 200);
+	      if (options.debug) console.log('COMBO response',combo);
+	      var output = {};
+	      combo.forEach(function(item){ output = extend(item.results[0], output); });
+	      if (options.debug) console.log('MERGE RESPONSE',output);
+	      return res.json({ results: [output] }, 200);
 	    }).catch((error) => {
-	      console.log(error);
-	      return res.send(error,500);
+	      if (options.debug) console.log(error);
+	      return res.json(error,500);
 	    });
 
 
@@ -91,14 +96,22 @@ module.exports = function HashFlux(options) {
   /* FALLBACK Handler */
   self.app.post('/*', (req, res) => {
     // Query All Servers, Aggregate responses
-    console.log('CATCHALL', req.url, req.body)
     Promise.all( servers.map(server => apiCall(req.body,req.url,server) )).then((combo) => {
-        console.log(combo);
-        return res.send(combo, 200);
+        if (options.debug) console.log('COMBO fallback',JSON.stringify(combo));
+	var output = {};
+        combo.forEach(function(item){ output = extend(item.results[0], output); });
+        if (options.debug) console.log('MERGE RESPONSE',output);
+        return res.json({ results: [output]}, 200);
       }).catch((error) => {
-        console.log(error);
-	return res.send(error,500);
+        if (options.debug) console.log(error);
+	return res.json(error,500);
       });
+  })
+
+  /* PING Handler */
+  self.app.get('/ping', (req, res) => {
+	if (options.debug) console.log('PING req', req);
+        return res.send('ok', 204);
   })
 
   self.app.listen(options.port||3000)
